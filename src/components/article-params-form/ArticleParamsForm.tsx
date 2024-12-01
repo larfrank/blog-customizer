a6a9b326a20734f495cd152f22b8e99d25b721e3
Lara Frank <lara.frank.frank@yandex.ru>
2024-12-02 01:04:55 +0300

    finish menu

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,9 +4,24 @@ import { clsx } from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 import { useState } from 'react';
 import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 export const ArticleParamsForm = () => {
 	const [isActive, setActive] = useState(false);
+	const [selectedFont, setFont] = useState(fontFamilyOptions[0]);
+	const [selectedFontSize, setFontSize] = useState(fontSizeOptions[0]);
+	const [selectedFontColor, setFontColor] = useState(fontColors[0]);
+	const [selectedBg, setBg] = useState(backgroundColors[0]);
+	const [selectedContentWidth, setContentWidth] = useState(contentWidthArr[0]);
 
 	return (
 		<>
@@ -24,6 +39,53 @@ export const ArticleParamsForm = () => {
 					<Text size={31} weight={800} uppercase={true}>
 						Задайте параметры
 					</Text>
+					<Separator />
+					<Select
+						selected={selectedFont}
+						options={fontFamilyOptions}
+						title='Шрифт'
+						onChange={(opt) => {
+							setFont(opt);
+						}}
+					/>
+					<Separator />
+					<RadioGroup
+						name='fontsize'
+						options={fontSizeOptions}
+						selected={selectedFontSize}
+						title='рАЗМЕР шрифта'
+						onChange={(opt) => {
+							setFontSize(opt);
+						}}
+					/>
+					<Separator />
+					<Select
+						selected={selectedFontColor}
+						options={fontColors}
+						title='Цвет шрифта'
+						onChange={(opt) => {
+							setFontColor(opt);
+						}}
+					/>
+					<Separator />
+					<Separator />
+					<Select
+						selected={selectedBg}
+						options={backgroundColors}
+						title='Цвет фона'
+						onChange={(opt) => {
+							setBg(opt);
+						}}
+					/>
+					<Separator />
+					<Select
+						selected={selectedContentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+						onChange={(opt) => {
+							setContentWidth(opt);
+						}}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
