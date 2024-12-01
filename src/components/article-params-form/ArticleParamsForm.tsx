0ecc31ee78d703ac78058c757389a25d4d9bd0a7
Lara Frank <lara.frank.frank@yandex.ru>
2024-12-01 22:59:54 +0300

    update article's form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,24 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { clsx } from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isActive, setActive] = useState(false);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isActive}
+				onClick={() => {
+					setActive(!isActive);
+				}}
+			/>
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isActive,
+				})}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
